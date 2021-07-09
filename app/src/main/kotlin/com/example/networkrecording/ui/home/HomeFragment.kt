bb30feda9f3db11488ed6c956a1c62606cbffc1b
Eric Liu <eric.liu@uber.com>
2021-07-09 16:51:54 +1000

    make the app works a little better

--- app/src/main/kotlin/com/example/networkrecording/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/com/example/networkrecording/ui/home/HomeFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.networkrecording.ItemViewModel
 import com.example.networkrecording.R
 import com.example.networkrecording.databinding.FragmentHomeBinding
 
@@ -15,6 +17,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private val viewModel: ItemViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,6 +38,12 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        viewModel.repos.observe(viewLifecycleOwner, Observer { repos ->
+            // Perform an action with the latest item data
+            textView.text = repos.toString()
+        })
+
         return root
     }
 
