bd61972ffd64572b0c67875121e5435c78b3b08d
Eric Liu <eric.liu@uber.com>
2021-07-14 00:09:36 +1000

    Protobuf serializer works

--- model-protobuf/src/main/kotlin/com/example/model/ProtoSerializer.kt
+++ model-protobuf/src/main/kotlin/com/example/model/ProtoSerializer.kt
@@ -1,35 +1,36 @@
-//package com.example.model
-//
-//import com.example.Serializer
-//import com.example.model.ProtoModelConverter.fromProtoRequest
-//import com.example.model.ProtoModelConverter.fromProtoResponse
-//import com.example.model.ProtoModelConverter.toProtoRequest
-//import com.example.model.ProtoModelConverter.toProtoResponse
-//import java.io.InputStream
-//import java.util.function.Consumer
-//
-//class ProtoSerializer : Serializer {
-//    override fun encodeToString(originalPair: Pair<BaseRequest, List<BaseResponse>>): ByteArray? {
-//        val protoRequest = toProtoRequest(originalPair.first)
-//        val protoResponses = mutableListOf<ProtoModel.Response>()
-//
-//        originalPair.second.forEach(Consumer { baseResponse ->
-//            protoResponses.add(toProtoResponse(baseResponse))
-//        })
-//
-//        return ProtoModel.Entry.newBuilder()
-//            .setRequest(protoRequest)
-//            .addAllResponses(protoResponses)
-//            .build()
-//            .toByteArray()
-//    }
-//
-//    override fun decodeFromString(inputStream: InputStream): Pair<BaseRequest, MutableList<BaseResponse>> {
-//        val entry = ProtoModel.Entry.parseFrom(inputStream)
-//
-//        val baseRequest: BaseRequest = fromProtoRequest(entry.request)
-//        val baseResponse: MutableList<BaseResponse> = entry.responsesList.map { response -> fromProtoResponse(response) }.toMutableList()
-//
-//        return Pair(baseRequest, baseResponse)
-//    }
-//}
+package com.example.model
+
+import com.example.Serializer
+import com.example.model.ProtoModelConverter.fromProtoRequest
+import com.example.model.ProtoModelConverter.fromProtoResponse
+import com.example.model.ProtoModelConverter.toProtoRequest
+import com.example.model.ProtoModelConverter.toProtoResponse
+import java.util.function.Consumer
+
+class ProtoSerializer : Serializer {
+    override fun encodeToByteArray(originalPair: Pair<BaseRequest, List<BaseResponse>>): ByteArray {
+        val protoRequest = toProtoRequest(originalPair.first)
+        val protoResponses = mutableListOf<ProtoModel.Response>()
+
+        originalPair.second.forEach(Consumer { baseResponse ->
+            protoResponses.add(toProtoResponse(baseResponse))
+        })
+
+        return ProtoModel.Entry.newBuilder()
+            .setRequest(protoRequest)
+            .addAllResponses(protoResponses)
+            .build()
+            .toByteArray()
+    }
+
+    override fun decodeFromByteArray(bytes: ByteArray): Pair<BaseRequest, MutableList<BaseResponse>> {
+        val entry = ProtoModel.Entry.parseFrom(bytes)
+
+        val baseRequest: BaseRequest = fromProtoRequest(entry.request)
+        val baseResponse: MutableList<BaseResponse> = entry.responsesList.map { response -> fromProtoResponse(response) }.toMutableList()
+
+        return Pair(baseRequest, baseResponse)
+    }
+
+
+}
